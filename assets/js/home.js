7450cec02dd4742dfb924aad9df2515bd46087c8
Lindon Jonhson <lindon.jonhsonf@gmail.com>
2016-04-12 14:37:11 -0300

    Added the already created content

---
+++ assets/js/home.js
@@ -0,0 +1,10 @@
+$('a#btn-menu h2').click(function(){
+	$('#menu_mobile #options').toggleClass('visible_options');
+});
+
+/*
+$('a#btn-menu h2').on('click touchstart', function(e){
+	$('#menu_mobile #options').toggleClass('#menu_mobile #options');
+  	e.preventDefault();
+});
+*/
