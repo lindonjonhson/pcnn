97570560a0921557efee6d687530e33c899973e1
Lindon Jonhson <lindon.jonhsonf@gmail.com>
2016-04-21 13:58:09 -0300

    Commentaries

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,3 +8,7 @@ $('a#btn-menu h2').on('click touchstart', function(e){
   	e.preventDefault();
 });
 */
+
+//ABOUT SENDING THE EMAIL
+//http://stackoverflow.com/questions/7381150/how-to-send-an-email-from-javascript
+//https://medium.com/@mariusc23/send-an-email-using-only-javascript-b53319616782#.k0xhvfprf
